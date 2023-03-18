7a347a11e69d9ec098eaf17abf64f37ec5ac16eb
Karapetyan Andres <key.andres@outlook.com>
2023-03-18 15:56:00 +0400

    solution task16

---
+++ Task16.py
@@ -0,0 +1,8 @@
+lst = []
+for i in range(int(input())):
+    lst.append(input())
+print(lst.count(input()))
+
+
+# x = [lst.append(input()) for i in range(int(input()))]
+# print(lst.count(input()))
